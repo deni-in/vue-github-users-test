3fef0ce561c09e7c3d7cddd19dbb5d6914e3fd6d
deni <deni.akhmatukaev@gmail.com>
2021-10-05 11:46:26 +0300

    vue-webpack-set

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">{{title}}</div>
+</template>
+
+<script>
+export default {
+  name: "App.vue",
+  data() {
+    return {
+      title: "test"
+    }
+  }
+}
+</script>
+
+<style>
+  .test {
+    color: red;
+  }
+</style>
